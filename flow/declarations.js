266c1b0eb523bfe68768b9632fec9215781c1485
PlusLius <408709581@qq.com>
2021-05-07 16:45:17 +0800

    Update declarations.js

--- flow/declarations.js
+++ flow/declarations.js
@@ -79,6 +79,9 @@ declare type RouteRecord = {
   props: boolean | Object | Function | Dictionary<boolean | Object | Function>;
 }
 
+// Vue-Router 中定义的 Location 数据结构和浏览器提供的 
+// window.location 部分结构有点类似，它们都是对 url 的结构化描述。举个例子：/abc?foo=bar&baz=qux#hello，
+// 它的 path 是 /abc，query 是 {foo:'bar',baz:'qux'}。Location 的其他属性我们之后会介绍。
 declare type Location = {
   _normalized?: boolean;
   name?: string;
@@ -92,6 +95,8 @@ declare type Location = {
 
 declare type RawLocation = string | Location
 
+// Route 表示的是路由中的一条线路，它除了描述了类似 Loctaion 的 path、query、hash 这些概念，
+// 还有 matched 表示匹配到的所有的 RouteRecord。
 declare type Route = {
   path: string;
   name: ?string;
