b4845da54bebe9286dc5ea1ea7639dd6b89cd9f8
PlusLius <408709581@qq.com>
2021-07-31 16:57:43 +0800

    Update async.js

--- src/util/async.js
+++ src/util/async.js
@@ -2,6 +2,9 @@
 // 官方的说法叫导航守卫，实际上就是发生在路由路径切换的时候，执行的一系列钩子函数。
 export function runQueue (queue: Array<?NavigationGuard>, fn: Function, cb: Function) {
 //    我们先从整体上看一下这些钩子函数执行的逻辑，首先构造一个队列 queue，它实际上是一个数组；
+//   这是一个非常经典的异步函数队列化执行的模式， queue 是一个 NavigationGuard 类型的数组
+//   我们定义了 step 函数，每次根据 index 从 queue 中取一个 guard，然后执行 fn 函数，并且把 guard 作为参数传入
+  // 第二个参数是一个函数，当这个函数执行的时候再递归执行 step 函数 前进到下一个，注意这里的 fn 就是我们刚才的 iterator 函数，那么我们再回到 iterator 函数的定义：
   const step = index => {
     if (index >= queue.length) {
       cb()
